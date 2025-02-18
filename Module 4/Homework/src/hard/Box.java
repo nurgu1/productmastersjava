d1ef786250ddb9d71dad3630da1b789ebb4ff6ff
nurgu1 <100270939+nurgu1@users.noreply.github.com>
2025-02-18 18:57:45 +0100

    Finished

--- Module 4/Homework/src/hard/Box.java
+++ Module 4/Homework/src/hard/Box.java
@@ -1,4 +1,33 @@
 package hard;
 
-public class Box {
+public class Box<T>{
+    private T item;
+
+    public Box(T item) {
+        this.item= item;
+    }
+
+    public void setItem(T item) {
+        this.item=item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void showType() {
+        System.out.println("Type of T:" + item.getClass().getName());
+    }
+}
+class MyData {
+    private String data;
+
+    public MyData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "MyData: " + data;
+    }
 }
--- Module 4/Homework/src/hard/Main.java
+++ Module 4/Homework/src/hard/Main.java
@@ -2,9 +2,14 @@ package hard;
 
 
 public class Main {
-
   public static void main(String[] args) {
 
-  }
+    Box<String> stringBox = new Box<>("Hello, Generics!");
+    System.out.println("Stored: " + stringBox.getItem());
+    stringBox.showType();
 
+    Box<MyData> dataBox = new Box<>(new MyData("Data Data Data"));
+    System.out.println("Stored: " + dataBox.getItem());
+    dataBox.showType();
+  }
 }
